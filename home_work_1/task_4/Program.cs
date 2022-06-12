f8bd97df31991deb00b53f110efaf87ff27c47a5
Dmitry <dvoroshin13@gmail.com>
2022-06-12 21:08:58 +0500

    четвертая задача решена

---
+++ home_work_1/task_4/Program.cs
@@ -0,0 +1,30 @@
+﻿// программа выводит все четные числа в диапазоне от 1 до N
+
+int num;
+
+Console.WriteLine("Введите целое число для вывода всех четных чисел в диапазоне от 1 до N");
+
+num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 0)
+{
+    while (num > 1)
+    {
+        if (num % 2 == 0)
+        {
+            Console.Write(num + " ");
+        }
+        num--;
+    }
+}
+else
+{
+    while (num < -1)
+    {
+        if (num % 2 == 0)
+        {
+            Console.Write(num + " ");
+        }
+        num++;
+    }
+}
