685166e5f16b8f60ad116b51ee4ed1605c98f2c1
Dmitry <dvoroshin13@gmail.com>
2022-06-12 20:58:15 +0500

    третья задача решена

---
+++ home_work_1/task_3/Program.cs
@@ -0,0 +1,16 @@
+﻿// программа определяет четное ли число
+
+int num;
+
+Console.WriteLine("Введите целое число для определения его четности");
+
+num = Convert.ToInt32(Console.ReadLine());
+
+if(num % 2 == 0)
+{
+    Console.WriteLine("Число четное!");
+}
+else
+{
+    Console.WriteLine("Число не четное :(");
+}
