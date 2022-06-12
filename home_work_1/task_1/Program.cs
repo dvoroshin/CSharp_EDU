b3c3aa381c7f5f45f6f58155e0fe98d7bcaa086d
Dmitry <dvoroshin13@gmail.com>
2022-06-12 20:30:35 +0500

    первая задача решена

--- home_work_1/task_1/Program.cs
+++ home_work_1/task_1/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿int val_1, val_2;
+
+Console.WriteLine("Введите последовательно два целочисленных значения");
+val_1 = Convert.ToInt32(Console.ReadLine());
+val_2 = Convert.ToInt32(Console.ReadLine());
+
+if (val_1 < val_2)
+{
+    Console.WriteLine("Меньшее значение:" + val_1);
+    Console.WriteLine("Большее значение:" + val_2);
+}
+else
+{
+    if (val_1 != val_2)
+    {
+        Console.WriteLine("Меньшее значение:" + val_2);
+        Console.WriteLine("Большее значение:" + val_1);
+    }
+    else
+    {
+        Console.WriteLine("Значения равны");
+    }
+
+}
