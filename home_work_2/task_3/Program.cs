d2ed1587bb68a10afe30b2b977eb7bf3a8dfcff0
Dmitry <dvoroshin13@gmail.com>
2022-06-20 21:54:55 +0500

    2-я домашка, третья задача решена!

---
+++ home_work_2/task_3/Program.cs
@@ -0,0 +1,36 @@
+﻿// программа определяет день недели и проверяет его на выходной
+
+Console.Clear();
+
+void dayHoly(int dayN)
+{
+    switch (dayN)
+    {
+        case 1: Console.WriteLine("Понедельник :("); break;
+        case 2: Console.WriteLine("Вторник :("); break;
+        case 3: Console.WriteLine("Среда :("); break;
+        case 4: Console.WriteLine("Четверг :("); break;
+        case 5: Console.WriteLine("Пятница :)"); break;
+        case 6: Console.WriteLine("Суббота!"); break;
+        case 7: Console.WriteLine("Воскресенье!"); break;
+        default: Console.WriteLine("Ошибка, попробуйте еще раз :)"); break;
+    }
+    holyDay(dayN);
+}
+
+void holyDay(int day)
+{
+    if (day == 6 || day == 7)
+        Console.WriteLine("Сегодня точно выходной, я проверил ;)");
+    else
+        if (day == 5)
+        Console.WriteLine("Не выходной, но почти ;)");
+    else
+        if (day == 1)
+        Console.WriteLine("Пора на работу!");
+    else
+        Console.WriteLine("Проверка показала, что сегодня не выходной :(");
+}
+
+Console.Write("Введите номер дня недели: ");
+dayHoly(Convert.ToInt32(Console.ReadLine()));
