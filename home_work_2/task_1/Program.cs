ed9c783069e7235c4ddda0b2cb5791b1bdd4bc62
Dmitry <dvoroshin13@gmail.com>
2022-06-20 20:58:17 +0500

    2-я домашка: первая задача решена

---
+++ home_work_2/task_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Программа показывает вторую цифру трехзначного числа
+
+Console.Clear();
+
+int get2num(int num)
+{
+    if (num > 99 && num < 1000)
+    {
+        return (num / 10) % 10;
+    }
+    else
+    {
+        Console.Write("Введено не трехзначное число, ошибка: ");
+        return -1;
+    }
+}
+
+Console.Write("Введите трехзначное число: ");
+Console.WriteLine(get2num(Convert.ToInt32(Console.ReadLine())));
