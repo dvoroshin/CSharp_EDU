57485e3d795363c4dca13b8c75b540efd1ac12b6
Dmitry <dvoroshin13@gmail.com>
2022-06-20 21:21:45 +0500

    2-я домашка: вторая задача решена

---
+++ home_work_2/task_2/Program.cs
@@ -0,0 +1,24 @@
+﻿// программа выводит третью цифру любого числа, если она есть
+
+Console.Clear();
+
+int get3num(int num)
+{
+    if (num > 99)
+    {
+        while (num > 999)
+        {
+            num /= 10;
+        }
+        Console.Write("Третья цифра числа: ");
+        return num % 10;
+    }
+    else
+    {
+        Console.Write("Введенное число не имеет третьей цифры, ошибка: ");
+        return -1;
+    }
+}
+
+Console.Write("Введите число: ");
+Console.WriteLine(get3num(Convert.ToInt32(Console.ReadLine())));
