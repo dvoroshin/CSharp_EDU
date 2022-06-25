484db3d206d84d04c44062b33dba43d39f983b1f
Dmitry <dvoroshin13@gmail.com>
2022-06-25 22:04:24 +0500

    3-я домашка 3 задача решена

---
+++ home_work_3/task_3/Program.cs
@@ -0,0 +1,13 @@
+﻿// программа выдает таблицу кубов для введенного числа от 1 до данного числа
+
+void getCube(int num)
+{
+    Console.Write(num + " -> ");
+    for (int i = 1; i <= num; i++)
+    {
+        Console.Write(Math.Pow(i,3) + " ");
+    }
+}
+
+Console.Write("Введите число N: ");
+getCube(Convert.ToInt32(Console.ReadLine()));
