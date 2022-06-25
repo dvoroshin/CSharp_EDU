41931aa7692f5c5d14ca35baac532577e8ce80e2
Dmitry <dvoroshin13@gmail.com>
2022-06-25 21:41:30 +0500

    3-я домашка, первая задача решена

---
+++ home_work_3/task_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// программа проверяет 5ти значное число на палиндром
+
+void numPoly(int num)
+{
+    if (num > 9999 && num < 100000)
+    {
+        if ((num / 10000 == num % 10) && ((num / 1000) % 10 == (num % 100) / 10))
+            Console.WriteLine(num + ": палиндром");
+        else
+            Console.WriteLine(num + ": не палиндром");
+    }
+    else
+        Console.WriteLine("Вы ввели не 5ти значное число");
+}
+
+Console.Write("Введите 5ти значное число: ");
+numPoly(Convert.ToInt32(Console.ReadLine()));
