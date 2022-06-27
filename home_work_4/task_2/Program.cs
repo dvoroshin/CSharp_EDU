b7e065048ff96f78d37d6fb1cd487b84c50cb98c
Dmitry <dvoroshin13@gmail.com>
2022-06-27 22:12:50 +0500

    4-я домашка, 2-я задача решена

---
+++ home_work_4/task_2/Program.cs
@@ -0,0 +1,29 @@
+﻿// программа вычисляет сумму цифр в числе
+
+int sumInNum(int num)
+{
+    if (num < 10) return num;
+
+    int result = 0;
+    int firstC = 0;
+
+    for (int i = num; i >= 10; i /= 10)
+    {
+        result += i % 10;
+        firstC = i / 10;
+    }
+
+    return result + firstC;
+}
+
+void askUser()
+{
+    Console.WriteLine("Введите число: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    if (num < 0)
+        Console.WriteLine(sumInNum(num * -1));
+    else
+        Console.WriteLine(sumInNum(num));
+}
+
+askUser();
