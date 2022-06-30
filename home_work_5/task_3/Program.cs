6b890d0d827478ba664b722460cdbacccfdac7b3
Dmitry <dvoroshin13@gmail.com>
2022-06-30 21:32:28 +0500

    5-я домашка, 3-я задача решена

---
+++ home_work_5/task_3/Program.cs
@@ -0,0 +1,51 @@
+﻿// ищет разницу между максимальным и минимальным вещественными элементами массива
+
+double[] GetArray(int size)
+{
+    Random rand = new Random();
+    if (size <= 0)
+        size = 1;
+    double[] array = new double[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rand.NextDouble() * 100;
+    }
+    return array;
+}
+
+void PrintArray(double[] array)
+{
+    Console.Write($"{Environment.NewLine}Исходный массив: [");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i],0:F2}, ");
+    }
+    Console.Write($"{array[array.Length - 1],0:F2}]{Environment.NewLine}");
+}
+
+void DifMinMax(double[] array)
+{
+    double min = double.MaxValue;
+    double max = double.MinValue;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < min) min = array[i];
+        if (array[i] > max) max = array[i];
+    }
+    Console.WriteLine($"Разница между максимальным и мнимальным элементами массива: {max - min,0:F2}");
+}
+
+double[] AskUser()
+{
+    Console.Clear();
+    Console.Write($"{Environment.NewLine}Введите размер массива: ");
+    int size = Convert.ToInt32(Console.ReadLine());
+    double[] array = GetArray(size);
+    PrintArray(array);
+
+    return array;
+}
+
+DifMinMax(AskUser());
