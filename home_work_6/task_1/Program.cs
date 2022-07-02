595daff3e806ae43a53c4637d7209961cfa42025
Dmitry <dvoroshin13@gmail.com>
2022-07-02 19:22:39 +0500

    6-я домашка, 1 задача решена

--- home_work_6/task_1/Program.cs
+++ home_work_6/task_1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// считает количество чисел бОльших нуля, которые вводит пользователь
+
+int GetNumOfConsole(int size)
+{
+    string? str = String.Empty;
+    int[] array = new int[size];
+    int sum = 0;
+    Console.WriteLine($"{Environment.NewLine}Последовательно введите числа ↓");
+    for (int i = 0; i < size; i++)
+    {
+        str = Console.ReadLine();
+        array[i] = Convert.ToInt32(str);
+        if(array[i] > 0)
+            sum += array[i];
+    }
+    return sum;
+}
+Console.Clear();
+Console.Write("Определите количество чисел для ввода: ");
+int mNum = Convert.ToInt32(Console.ReadLine());
+int result = GetNumOfConsole(mNum);
+Console.Write($"{Environment.NewLine}Сумма введеных чисел со значением больше нуля: {result}{Environment.NewLine}");
