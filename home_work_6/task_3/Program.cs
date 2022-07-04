4eaaaeb58d2fc7ebcb4cd217a0b69fae0f67ab0a
Dmitry Voroshin <dvoroshin13@gmail.com>
2022-07-04 12:19:11 +0500

    6-я домашка, 3-я задача решена

--- home_work_6/task_3/Program.cs
+++ home_work_6/task_3/Program.cs
@@ -1,13 +1,29 @@
-﻿// 
+﻿// находит координаты точки пересечения прямых
 
+void GetInterLine(double k1, double b1, double k2, double b2)
+{
+    if(k1==k2 && b1 != b2)
+        Console.WriteLine("прямые параллельны!");
+    else
+        if(k1==k2 && b1 == b2)
+            Console.WriteLine("прямые совпадают!");
+        else
+        {
+            double x = (b2 - b1)/(k1 - k2);
+            double y = k1*x + b1;
+            Console.WriteLine($"Координаты точки пересечения прямых: x : {x} | y : {y}");
+        }
+    
+}
+Console.Clear();
 double k1, k2, b1, b2;
-
+Console.Write("Введите коэффициент k для прямой #1: ");
 k1 = Convert.ToDouble(Console.ReadLine());
-k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите коэффициент b для прямой #1: ");
 b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите коэффициент k для прямой #2: ");
+k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите коэффициент b для прямой #2: ");
 b2 = Convert.ToDouble(Console.ReadLine());
 
-double x = (b2 - b1)/(k1 - k2);
-double y = k1*x + b1;
-
-Console.WriteLine($"{x}, {y}");
+GetInterLine(k1, b1, k2, b2);
