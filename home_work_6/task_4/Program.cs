ad6edc00ed38abad9996326615894c921e117f73
Dmitry Voroshin <dvoroshin13@gmail.com>
2022-07-05 12:08:15 +0500

    6-я домашка, 4-я задача решена

--- home_work_6/task_4/Program.cs
+++ home_work_6/task_4/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// выводит последовательность Фибоначчи (Леонардо Пизанского) для указанного количества чисел
+
+void PrintFibonachi(int num)
+{
+    double sum = 0;
+    double first = 0;
+    double second = 1;
+    int count = 0;
+    Console.WriteLine($"{Environment.NewLine}Итоговая последовательность Фибоначчи из {num} чисел: {Environment.NewLine}");
+    while (count < num)
+    {
+        Console.Write("{0} ", sum);
+        count++;
+        first = second;
+        second = sum;
+        sum = second + first;
+    }
+}
+
+Console.Clear();
+Console.Write("Введите количество чисел, которое необходимо вывести, согласно последовательности Фибоначчи: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+PrintFibonachi(num);
