577ff835178653b54767765639c465435f6a00ca
Dmitry <dvoroshin13@gmail.com>
2022-07-07 15:34:51 +0500

    7-я домашка 1-я задача решена

---
+++ home_work_7/task_1/Program.cs
@@ -0,0 +1,37 @@
+﻿// создает двумерный массив рандомных вещественных чисел с размером MxN 
+
+double[,] CreateRandomArray(int m, int n)
+{
+    double[,] array = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = new Random().NextDouble() * 100;
+        }
+    }
+    return array;
+}
+
+void ShowArray(double[,] array)
+{
+    Console.WriteLine();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j], 0:F2}\t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+Console.Clear();
+
+Console.Write("Enter number of rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter number of columns: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+ShowArray(CreateRandomArray(rows, columns));
