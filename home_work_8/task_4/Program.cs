610d4e8f7f5ed34174176cc0bf3415475f648458
Dmitry <dvoroshin13@gmail.com>
2022-07-12 20:56:18 +0500

    8-я домашка, 4-я задача решена

--- home_work_8/task_4/Program.cs
+++ home_work_8/task_4/Program.cs
@@ -1,11 +1,10 @@
-﻿// создает трехмерный массив из неповторяющихся чисел
+﻿// создает трехмерный массив из неповторяющихся двузначных чисел
 
-int[,,] CreateRandomArrayThree(int x, int y, int z, int min, int max)
+int[,,] CreateRandomArrayThree(int x, int y, int z)
 {
     int[,,] arrayThree = new int[x, y, z];
     int[] arrayForRepeat = new int[x * y * z];
-    int temp;
-    bool flagNotRepeat = false;
+    int min = 10, max = 99; //т.к двузначные числа
 
     for (int i = 0; i < x; i++)
     {
@@ -13,21 +12,48 @@ int[,,] CreateRandomArrayThree(int x, int y, int z, int min, int max)
         {
             for (int k = 0; k < z; k++)
             {
-                temp = new Random().Next(min, max + 1);
-                for (int l = i+j+k; l >= 0; l--)
+                arrayThree[i, j, k] = min;
+                min++;
+                if(min == max)
                 {
-                    if(arrayForRepeat[l] != temp)
-                    {
-                        flagNotRepeat = true;
-                    }
+                    min *= -1;
+                    max = -10;
                 }
-                if(flagNotRepeat)
-                    arrayThree[i,j,k] = temp;
-                else
-                    k--;
             }
         }
     }
     return arrayThree;
 }
 
+void ShowArrayThree(int[,,] arrayThree, string nameArray)
+{
+    Console.WriteLine($"{Environment.NewLine}{nameArray}");
+    for (int i = 0; i < arrayThree.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrayThree.GetLength(1); j++)
+        {
+            for (int k = 0; k < arrayThree.GetLength(2); k++)
+            {
+                Console.Write($"[{i},{j},{k}]{arrayThree[i, j, k]}\t");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+Console.Clear();
+
+Console.Write("Enter number of rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter number of columns: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter number of bar: ");
+int bar = Convert.ToInt32(Console.ReadLine());
+
+int[,,] array = CreateRandomArrayThree(rows, columns, bar);
+
+ShowArrayThree(array, "Трехмерный массив:");
