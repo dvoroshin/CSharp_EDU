0ea0601c24aa07a2663d47a496b11b13d15e2c44
Dmitry Voroshin <dvoroshin13@gmail.com>
2022-07-12 17:02:04 +0500

    4-я задачка промежуточное решение

---
+++ home_work_8/task_4/Program.cs
@@ -0,0 +1,33 @@
+﻿// создает трехмерный массив из неповторяющихся чисел
+
+int[,,] CreateRandomArrayThree(int x, int y, int z, int min, int max)
+{
+    int[,,] arrayThree = new int[x, y, z];
+    int[] arrayForRepeat = new int[x * y * z];
+    int temp;
+    bool flagNotRepeat = false;
+
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < y; j++)
+        {
+            for (int k = 0; k < z; k++)
+            {
+                temp = new Random().Next(min, max + 1);
+                for (int l = i+j+k; l >= 0; l--)
+                {
+                    if(arrayForRepeat[l] != temp)
+                    {
+                        flagNotRepeat = true;
+                    }
+                }
+                if(flagNotRepeat)
+                    arrayThree[i,j,k] = temp;
+                else
+                    k--;
+            }
+        }
+    }
+    return arrayThree;
+}
+
